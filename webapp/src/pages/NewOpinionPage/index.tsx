ff76139c125cf9c276af0ef9a960f11d4d40ff61
pudgekaramelkin <whowiewas@gmail.com>
2025-01-12 16:58:29 +0300

    Add success mesage

--- webapp/src/pages/NewOpinionPage/index.tsx
+++ webapp/src/pages/NewOpinionPage/index.tsx
@@ -1,12 +1,15 @@
 import { zCreateOpinionTrpcInput } from '@unpopularopinion/backend/src/router/createOpinion/input'
 import { useFormik } from 'formik'
 import { withZodSchema } from 'formik-validator-zod'
+import { useState } from 'react'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
 import { trpc } from '../../lib/trpc'
 
 export const NewOpinionPage = () => {
+  const [successMessageVisible, setSuccessMessageVisible] = useState(false)
+
   const createOpinion = trpc.createOpinion.useMutation()
 
   const formik = useFormik({
@@ -19,6 +22,11 @@ export const NewOpinionPage = () => {
     validate: withZodSchema(zCreateOpinionTrpcInput),
     onSubmit: async (values) => {
       await createOpinion.mutateAsync(values)
+      formik.resetForm()
+      setSuccessMessageVisible(true)
+      setTimeout(() => {
+        setSuccessMessageVisible(false)
+      }, 3000)
     },
   })
 
@@ -35,6 +43,7 @@ export const NewOpinionPage = () => {
         <Input name="description" label="description" formik={formik} />
         <Textarea name="text" label="text" formik={formik} />
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>some fields are invalid.</div>}
+        {successMessageVisible && <div style={{ color: 'green' }}>opinion created!</div>}
         <button type="submit" disabled={formik.isSubmitting}>
           {formik.isSubmitting ? 'submitting...' : 'create opinion'}
         </button>
