b5cddb8fa7f5cab9303893ed35ea59f26a648b33
pudgekaramelkin <whowiewas@gmail.com>
2025-01-12 18:46:49 +0300

    Add FormItems component

--- webapp/src/pages/NewOpinionPage/index.tsx
+++ webapp/src/pages/NewOpinionPage/index.tsx
@@ -4,6 +4,7 @@ import { withZodSchema } from 'formik-validator-zod'
 import { useState } from 'react'
 import { Alert } from '../../components/Alert'
 import { Button } from '../../components/Button'
+import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
@@ -48,14 +49,16 @@ export const NewOpinionPage = () => {
           formik.handleSubmit()
         }}
       >
-        <Input name="name" label="name" formik={formik} />
-        <Input name="nick" label="nick" formik={formik} />
-        <Input name="description" label="description" formik={formik} maxWidth={500} />
-        <Textarea name="text" label="text" formik={formik} />
-        {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>some fields are invalid.</div>}
-        {!!submittingError && <Alert color="red">{submittingError}</Alert>}
-        {successMessageVisible && <Alert color="green">opinion created!</Alert>}
-        <Button loading={formik.isSubmitting}>create opinion</Button>
+        <FormItems>
+          <Input name="name" label="name" formik={formik} />
+          <Input name="nick" label="nick" formik={formik} />
+          <Input name="description" label="description" formik={formik} maxWidth={500} />
+          <Textarea name="text" label="text" formik={formik} />
+          {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>some fields are invalid.</div>}
+          {!!submittingError && <Alert color="red">{submittingError}</Alert>}
+          {successMessageVisible && <Alert color="green">opinion created!</Alert>}
+          <Button loading={formik.isSubmitting}>create opinion</Button>
+        </FormItems>
       </form>
     </Segment>
   )
