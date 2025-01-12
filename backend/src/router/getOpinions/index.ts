ac73df1a11ecbe1d2cfa773e77d04542b4703b8a
pudgekaramelkin <whowiewas@gmail.com>
2025-01-12 16:14:42 +0300

    Fix huksy work

--- backend/src/router/getOpinions/index.ts
+++ backend/src/router/getOpinions/index.ts
@@ -1,7 +1,7 @@
-import _ from "lodash"
-import { opinions } from "../../lib/ideas"
-import { trpc } from "../../lib/trpc"
+import _ from 'lodash'
+import { opinions } from '../../lib/ideas'
+import { trpc } from '../../lib/trpc'
 
 export const getOpinionsTrpcRoute = trpc.procedure.query(() => {
-    return { opinions: opinions.map((opinion) => _.pick(opinion, ['nick', 'name', 'description'])) }
-  })
+  return { opinions: opinions.map((opinion) => _.pick(opinion, ['nick', 'name', 'description'])) }
+})
--- webapp/src/pages/NewOpinionPage/index.tsx
+++ webapp/src/pages/NewOpinionPage/index.tsx
@@ -15,8 +15,14 @@ export const NewOpinionPage = () => {
     },
     validate: withZodSchema(
       z.object({
-        name: z.string().min(1).regex(/^[a-z0-9].*\.$/, 'name first letter must be in lower case and last letter must be a dot.'),
-        nick: z.string().min(1).regex(/^[a-z0-9-]+$/, 'nick may contain only lowercase letters, numbers and dashes.'),
+        name: z
+          .string()
+          .min(1)
+          .regex(/^[a-z0-9].*\.$/, 'name first letter must be in lower case and last letter must be a dot.'),
+        nick: z
+          .string()
+          .min(1)
+          .regex(/^[a-z0-9-]+$/, 'nick may contain only lowercase letters, numbers and dashes.'),
         description: z.string().min(1, 'description should be at least 1 characters long.'),
         text: z.string().min(100, 'text should be at least 100 characters long.'),
       })
