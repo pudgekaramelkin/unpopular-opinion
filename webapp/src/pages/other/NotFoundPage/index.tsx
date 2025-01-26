eeeb2a723a3a965981034f2ca3353313d9583f43
pudgekaramelkin <whowiewas@gmail.com>
2025-01-26 22:31:54 +0300

    Add 404 page

--- webapp/src/pages/other/NotFoundPage/index.tsx
+++ webapp/src/pages/other/NotFoundPage/index.tsx
@@ -1,9 +1,9 @@
+import image404 from '../../../assets/images/404.png'
 import { ErrorPageComponent } from '../../../components/ErrorPageComponent'
+import styles from './index.module.scss'
 
-export const NotFoundPage = ({
-  title = 'not found',
-  message = 'this page does not exist',
-}: {
-  title?: string
-  message?: string
-}) => <ErrorPageComponent title={title} message={message} />
+export const NotFoundPage = ({ title = 'not found', message }: { title?: string; message?: string }) => (
+  <ErrorPageComponent title={title} message={message}>
+    <img src={image404} className={styles.image} alt="" width="800" height="600" />
+  </ErrorPageComponent>
+)
